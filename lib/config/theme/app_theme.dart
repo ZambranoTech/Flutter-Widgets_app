334e0e769b4441f6c502ab54b1f9646ebb1d3216
Javier García <a2027garciaj@gmail.com>
2023-10-03 22:03:22 +0200

    statenotifier implementado

--- lib/config/theme/app_theme.dart
+++ lib/config/theme/app_theme.dart
@@ -16,17 +16,23 @@ class AppTheme {
   final int selectedColor;
   final bool isDarkMode;
 
-  AppTheme({
-    this.selectedColor = 0,
-    this.isDarkMode = false
-  })
+  AppTheme({this.selectedColor = 0, this.isDarkMode = false})
       : assert(selectedColor >= 0, 'Selected colors must be greater than 0'),
         assert(selectedColor < colorList.length,
             'Selected colors must be less or equal than ${colorList.length - 1}');
 
   ThemeData getTheme() => ThemeData(
       useMaterial3: true,
-      brightness: isDarkMode? Brightness.dark : Brightness.light,
+      brightness: isDarkMode ? Brightness.dark : Brightness.light,
       colorSchemeSeed: colorList[selectedColor],
       appBarTheme: const AppBarTheme(centerTitle: true));
+
+  AppTheme copyWith({
+    int? selectedColor,
+    bool? isDarkMode
+
+  }) => AppTheme(
+    selectedColor: selectedColor ?? this.selectedColor,
+    isDarkMode: isDarkMode ?? this.isDarkMode,
+    );
 }
--- lib/presentation/screens/theme_changer/theme_changer.dart
+++ lib/presentation/screens/theme_changer/theme_changer.dart
@@ -9,7 +9,7 @@ class ThemeChangerScreen extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, ref) {
-    final isDarkMode = ref.watch(isDarkModeProvider);
+    final isDarkMode = ref.watch(themeNotifierProvider).isDarkMode;
 
     return Scaffold(
       appBar: AppBar(
@@ -20,7 +20,8 @@ class ThemeChangerScreen extends ConsumerWidget {
                   ? const Icon(Icons.light_mode_outlined)
                   : const Icon(Icons.dark_mode_outlined),
               onPressed: () {
-                ref.read(isDarkModeProvider.notifier).update((state) => !state);
+                // ref.read(isDarkModeProvider.notifier).update((state) => !state);
+                ref.read(themeNotifierProvider.notifier).toggleDarkMode();
               }),
         ],
       ),
@@ -35,7 +36,8 @@ class _ThemeChangerView extends ConsumerWidget {
   @override
   Widget build(BuildContext context, ref) {
     final List<Color> colors = ref.watch(colorListProvider);
-    final selectedColor = ref.watch(selectedColorProvider);
+    final selectedColor = ref.watch(themeNotifierProvider).selectedColor;
+    // final int selectedColor = ref.watch( selectedColorProvider );
 
     return ListView.builder(
       itemCount: colors.length,
@@ -52,7 +54,7 @@ class _ThemeChangerView extends ConsumerWidget {
           value: index,
           groupValue: selectedColor,
           onChanged: (value) {
-            ref.read(selectedColorProvider.notifier).state = index;
+            ref.read(themeNotifierProvider.notifier).changeColorIndex(index);
           },
         );
       },
